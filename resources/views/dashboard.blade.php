8298e43dcd524fbfd79d2fcd4d1fe4612a334266
Diveritus <diana.islas5367@alumnos.udg.mx>
2023-11-30 21:56:30 -0600

    ll

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,19 @@
-<x-adminlte::page>
-    <x-slot name="title">
-        Dashboard
-    </x-slot>
+@extends('adminlte::page')
 
-    <x-slot name="content_header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@section('title', 'Dashboard')
 
-    <x-slot name="content">
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <x-welcome />
-                </div>
-            </div>
-        </div>
-    </x-slot>
-</x-adminlte::page>
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p> Welcome.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
