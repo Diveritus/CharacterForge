62a14cca8b076a8a19993280ef79888c51b7fd97
Diveritus <diana.islas5367@alumnos.udg.mx>
2023-11-30 21:51:12 -0600

    vlnm

--- resources/views/Personaje/index.blade.php
+++ resources/views/Personaje/index.blade.php
@@ -1,121 +1,13 @@
-{{-- @extends('layouts.base')
-
-@section('content')
-<div class="row">
-    <div class="col-12">
-        <div>
-            <x-adminlte-button label="Crear" theme="primary" href="{{ route('personajes.create') }}" />
-        </div>
-    </div>
-
-    @if (Session::get('success'))
-        <x-adminlte-alert theme="success" title="Éxito">
-            {{ Session::get('success') }}
-        </x-adminlte-alert>
-    @endif
-
-    <div class="col-12 mt-4 flex flex-wrap">
-        @foreach ($personajes as $personaje)
-            <div class="card w-full bg-base-100 text-black shadow-xl mb-4 mx-2">
-                <div class="card-body p-4">
-                    <h2 class="card-title text-lg font-semibold">{{ $personaje->nombre }}</h2>
-                    <p class="text-sm">{{ $personaje->clase->name }}</p>
-                    <p class="text-sm">{{ $personaje->raza->name }}</p>
-                </div>
-                <div class="card-actions p-4 border-t border-base-200">
-                    <x-adminlte-button label="Editar" theme="warning" class="text-xs" href="{{ route('personajes.edit', $personaje->id) }}" />
-                    <x-adminlte-button label="Detalles" theme="info" class="text-xs" href="{{ route('personajes.show', $personaje->id) }}" />
-                    
-                    @if($personaje->trashed())
-                        <form action="{{ route('personajes.restore', $personaje->id) }}" method="POST" class="d-inline">
-                            @csrf
-                            @method('PUT')
-                            <x-adminlte-button type="submit" label="Restaurar" theme="success" class="text-xs" />
-                        </form>
-                    @else
-                        <form action="{{ route('personajes.destroy', $personaje->id) }}" method="POST" class="d-inline">
-                            @csrf
-                            @method('DELETE')
-                            <x-adminlte-button type="submit" label="Eliminar" theme="danger" class="text-xs" />
-                        </form>
-                    @endif
-                </div>
-            </div>
-        @endforeach
-    </div>
-</div>
-@endsection --}}
-
-
-
-
+{{-- 
 @extends('adminlte::page')
 
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Personajes</h1>
+    <h1>DND Character</h1>
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <x-adminlte-button label="Crear" theme="primary" href="{{ route('personajes.create') }}" />
-        </div>
-
-        @if (Session::get('success'))
-            <x-adminlte-alert theme="success" title="Éxito">
-                {{ Session::get('success') }}
-            </x-adminlte-alert>
-        @endif
-
-        <div class="col-12 mt-4">
-            <div class="card-group">
-                @foreach ($personajes as $personaje)
-                    <div class="card bg-base-100 text-black shadow-xl mb-4 mx-2" style="min-width: 200px;">
-                        <div class="card-body p-4">
-                            <h2 class="card-title text-lg font-semibold">{{ $personaje->nombre }}</h2>
-                            <p class="text-sm">{{ $personaje->clase->name }}</p>
-                            <p class="text-sm">{{ $personaje->raza->name }}</p>
-                        </div>
-                        <div class="card-actions p-4 border-t border-base-200 d-flex justify-content-between">
-                            <x-adminlte-button label="Editar" theme="warning" class="text-xs" href="{{ route('personajes.edit', $personaje->id) }}" />
-                            <x-adminlte-button label="Detalles" theme="info" class="text-xs" href="{{ route('personajes.show', $personaje->id) }}" />
-
-                            @if($personaje->trashed())
-                                <form action="{{ route('personajes.restore', $personaje->id) }}" method="POST" class="d-inline">
-                                    @csrf
-                                    @method('PUT')
-                                    <x-adminlte-button type="submit" label="Restaurar" theme="success" class="text-xs" />
-                                </form>
-                            @else
-                                <form action="{{ route('personajes.destroy', $personaje->id) }}" method="POST" class="d-inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <x-adminlte-button type="submit" label="Eliminar" theme="danger" class="text-xs" />
-                                </form>
-                            @endif
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
-@endsection
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
-
-
-
-
-
-{{-- @section('content')
 <div class="row">
     <div class="col-12">
         <div>
@@ -125,7 +17,7 @@
 
     @if (Session::get('success'))
         <div class="alert alert-success mt-2">
-            <strong>{{ Session::get('success') }} <br>
+            <strong>{{ Session::get('success') }} <br></strong>
         </div>
     @endif
 
@@ -145,26 +37,79 @@
                     <td>
                         <a href="{{ route('personajes.edit', $personaje->id) }}" class="btn btn-warning">Editar</a>
                         <a href="{{ route('personajes.show', $personaje->id) }}" class="btn btn-info">Detalles</a>
-        
-                        @if($personaje->trashed())
-                            <form action="{{ route('personajes.restore', $personaje->id) }}" method="POST" class="d-inline">
-                                @csrf
-                                <!-- Agrega el método DELETE para la restauración -->
-                                @method('PUT')
-                                <button type="submit" class="btn btn-success">Restaurar</button>
-                            </form>
-                        @else
-                            <form action="{{ route('personajes.destroy', $personaje->id) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Eliminar</button>
-                            </form>
-                        @endif
-                    
+
+                        <form action="{{ route('personajes.destroy', $personaje->id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Eliminar</button>
+                        </form>
                     </td>
                 </tr>
             @endforeach
         </table>
     </div>
 </div>
-@endsection --}}
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop --}}
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>DND Character</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <a href="{{ route('personajes.create') }}" class="btn btn-primary">Crear</a>
+        </div>
+
+        @if (Session::get('success'))
+            <div class="alert alert-success mt-2">
+                <strong>{{ Session::get('success') }} <br></strong>
+            </div>
+        @endif
+
+        <div class="col-12 mt-4">
+            <div class="row">
+                @foreach ($personajes as $personaje)
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $personaje->nombre }}</h5>
+                                <p class="card-text"><strong>Clase:</strong> {{ $personaje->clase->name }}</p>
+                                <p class="card-text"><strong>Raza:</strong> {{ $personaje->raza->name }}</p>
+                                <div class="btn-group" role="group" aria-label="Acciones">
+                                    <a href="{{ route('personajes.edit', $personaje->id) }}" class="btn btn-warning">Editar</a>
+                                    <a href="{{ route('personajes.show', $personaje->id) }}" class="btn btn-info">Detalles</a>
+                                    <form action="{{ route('personajes.destroy', $personaje->id) }}" method="POST" class="d-inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
--- resources/views/Personaje/show.blade.php
+++ resources/views/Personaje/show.blade.php
@@ -1,31 +1,39 @@
-@extends('layouts.base')
+@extends('adminlte::page')
+
+@section('title', 'Detalles del Personaje')
+
+@section('content_header')
+    <h1>Detalles del Personaje</h1>
+@stop
 
 @section('content')
 <div class="row">
-    
-    <div class="col-12 mt-2">
-        <a href="{{ route('personajes.index') }}" class="btn btn-primary btn-sm">
-            <i class="fa fa-arrow-left"></i> Volver
-        </a>
+    <div class="col-12">
+        <div>
+            <a href="{{ route('personajes.index') }}" class="btn btn-primary">Volver</a>
+        </div>
     </div>
-    <div class="col-12 mt-2">
+
+    <div class="col-12 mt-4">
         <div class="card">
             <div class="card-body">
-                
-                <p class="card-text"><strong>Nombre:</strong> {{ $personaje->nombre }}</p>
-                <p class="card-text"><strong>Raza:</strong> {{ $personaje->raza }}</p>
-                <p class="card-text"><strong>Clase:</strong> {{ $personaje->clase }}</p>
-                <p class="card-text"><strong>Puntos de Golpe:</strong> {{ $personaje->puntos_de_golpe }}</p>
-                <p class="card-text"><strong>Puntos de Golpe Temporales:</strong> {{ $personaje->puntos_de_golpe_temporales }}</p>
-                <p class="card-text"><strong>Trasfondo:</strong> {{ $personaje->trasfondo }}</p>
-                <p class="card-text"><strong>Historia:</strong> {{ $personaje->historia }}</p>
-                <p class="card-text"><strong>Habilidades Especiales:</strong> {{ $personaje->habilidades_especiales }}</p>
-                <p class="card-text"><strong>Objetos Mágicos:</strong> {{ $personaje->objetos_magico }}</p>
+                <h2 class="card-title">{{ $personaje->nombre }}</h2>
+                <p class="card-text"><strong>Raza:</strong> {{ $personaje->raza ? $personaje->raza->name : 'N/A' }}</p>
+
+                <p class="card-text"><strong>Clase:</strong> {{ $personaje->clase ? $personaje->clase->name : 'N/A' }}</p>
+
+                <p class="card-text"><strong>Subclase:</strong> {{ $personaje->subclase ? $personaje->subclase->name : 'N/A' }}</p>
+                <p class="card-text"><strong>Nivel:</strong> {{ $personaje->nivel }}</p>
+                <p class="card-text"><strong>Fuerza:</strong> {{ $personaje->stats['fuerza'] }}</p>
+                <p class="card-text"><strong>Destreza:</strong> {{ $personaje->stats['destreza'] }}</p>
+                <p class="card-text"><strong>Constitución:</strong> {{ $personaje->stats['constitucion'] }}</p>
+                <p class="card-text"><strong>Inteligencia:</strong> {{ $personaje->stats['inteligencia'] }}</p>
+                <p class="card-text"><strong>Sabiduría:</strong> {{ $personaje->stats['sabiduria'] }}</p>
+                <p class="card-text"><strong>Carisma:</strong> {{ $personaje->stats['carisma'] }}</p>
+                <p class="card-text"><strong>Trasfondo:</strong> {{ $personaje->background ? $personaje->background->name : 'N/A' }}</p>
+                <p class="card-text"><strong>Descripción:</strong> {{ $personaje->descripcion }}</p>
             </div>
         </div>
     </div>
 </div>
-
-<button class="btn">Button</button>
 @endsection
-
